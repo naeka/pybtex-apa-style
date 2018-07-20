6f7f09643432154386d31b1eee0fd0a89f7db334
Léo S <leo@naeka.fr>
2018-07-20 17:26:46 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name='pybtex-apa-style',
+    version='1',
+    author='Naeka',
+    author_email='contact@naeka.fr',
+    description='Pybtex APA-like style',
+    url='https://github.com/naeka/pybtex-apa-style',
+    py_modules=['formatting.apa', 'labels.apa'],
+    entry_points={
+        'pybtex.style.formatting': 'apa = formatting.apa:APAStyle',
+        'pybtex.style.labels': 'apa = labels.apa:LabelStyle',
+    },
+    classifiers=(
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Utilities',
+    ),
+)
